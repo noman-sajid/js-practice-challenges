ebeb4635579ea942701f9b985d2470b49ef5f999
noman-sajid <noman15072002@gmail.com>
2025-08-28 20:45:27 +0500

    Problem-26-updated

--- Problem-26/Solution.js
+++ Problem-26/Solution.js
@@ -1,19 +1,38 @@
  //count characters and return an object
-function countChars(str){
-   const charFreq = {}
 
-   for(char of str){
-    if(charFreq[char]){
-      charFreq[char]++;
-    } else{
-      charFreq[char] = 1;
-    }
+ //Using loop
+// function countChars(str){
+//    const charFreq = {}
+
+//    for(char of str){
+//     if(charFreq[char]){
+//       charFreq[char]++;
+//     } else{
+//       charFreq[char] = 1;
+//     }
   
-   }
-    return charFreq;
+//    }
+//     return charFreq;
+// }
+
+
+// const sentance = "someone tell this guy"
+// const result = countChars(sentance);
+// console.log(result);
+
+
+
+//Using reduce method
+
+
+ 
+function countChars(str){
+   return str.split('').reduce((charFreq, char) => {
+      charFreq[char] = (charFreq[char] || 0) + 1;
+      return charFreq;
+   }, {})
 }
 
-
-const sentance = "someone tell this guy"
-const result = countChars(sentance);
+const sentence = "someone tell this guy";
+const result = countChars(sentence);
 console.log(result);
