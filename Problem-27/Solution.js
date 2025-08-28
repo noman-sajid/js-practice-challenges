292fbcc2256c675cfb9ea2cb1be1b1408537fb1c
noman-sajid <noman15072002@gmail.com>
2025-08-28 22:12:59 +0500

    Problem-27-updated

--- Problem-27/Solution.js
+++ Problem-27/Solution.js
@@ -1,8 +1,29 @@
 // Using a method
- function calcAvg(numbers) {
-  const sum = numbers.reduce((acc, crrVal) => acc + crrVal, 0);
-  return sum / numbers.length;
+//  function calcAvg(numbers) {
+//   const sum = numbers.reduce((acc, crrVal) => acc + crrVal, 0);
+//   return sum / numbers.length;
+// }
+// const nums = [10, 20, 30, 40, 50]; 
+// const avg = calcAvg(nums);
+//  console.log("Average:", avg);
+
+
+// Using a Loop
+
+
+function calcAvgWithLoop(numbers){
+  let sum = 0
+  for(let i = 0; i < numbers.length; i++){
+    sum = sum + numbers[i]
+    
+  }
+  const average = sum / numbers.length
+  return average 
 }
-const nums = [10, 20, 30, 40, 50]; 
-const avg = calcAvg(nums);
- console.log("Average:", avg);
+
+
+
+// Do not edit below this line
+const nums = [10, 20, 30, 40, 50];
+const avg = calcAvgWithLoop(nums);
+console.log("Average:", avg);
