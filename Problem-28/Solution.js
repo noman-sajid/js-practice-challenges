3aff35b15725dc581627857c2468d18467b77cfe
noman-sajid <noman15072002@gmail.com>
2025-08-29 21:36:24 +0500

    Problem-28-updated

--- Problem-28/Solution.js
+++ Problem-28/Solution.js
@@ -1,12 +1,33 @@
 //using methods
+// function avgLength(string){
+//     const elements = string.split(" ");
+//     let lengths = elements.map(word => word.length).reduce((acc , crrVal)=> acc + crrVal);
+//   const totalAvg = lengths/elements.length
+// return totalAvg
+
+// }
+
+// const sentance = "Hello, world!";
+// const avg = avgLength(sentance);
+// console.log("Average Length:", avg);
+
+
+//Using a Loop
+
 function avgLength(string){
     const elements = string.split(" ");
-    let lengths = elements.map(word => word.length).reduce((acc , crrVal)=> acc + crrVal);
-  const totalAvg = lengths/elements.length
-return totalAvg
-
+    let totalLength = 0;
+    
+    for(element of elements){
+      const lengths = element.length;
+      totalLength = totalLength + lengths
+      
+    }
+    const totalAvg = totalLength / elements.length;
+    return totalAvg
 }
 
 const sentance = "Hello, world!";
+
 const avg = avgLength(sentance);
-console.log("Average Length:", avg);
+console.log("Average Length:", avg); 
