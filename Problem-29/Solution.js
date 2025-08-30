9987e29803bff2f42288834795f1017427ac22a7
noman-sajid <noman15072002@gmail.com>
2025-08-30 21:30:47 +0500

    Problem-29-updated

--- Problem-29/Solution.js
+++ Problem-29/Solution.js
@@ -1,17 +1,33 @@
 // Write a function longestWord that takes a string (a sentence) and returns the longest word in it.
 //using functions
-function avgLength(string){
-   const words = string.replace(/[^\w\s]/g, '')
-   const cleanWords = words.split(" ")
-   const lengths = cleanWords.map(word => word.length)
-   const longest = Math.max(...lengths)
-   console.log(cleanWords)
-   const matchingWord = cleanWords.find( cleanWord => cleanWord.length === longest)
-   return matchingWord
+// function avgLength(string){
+//    const words = string.replace(/[^\w\s]/g, '')
+//    const cleanWords = words.split(" ")
+//    const lengths = cleanWords.map(word => word.length)
+//    const longest = Math.max(...lengths)
+//    console.log(cleanWords)
+//    const matchingWord = cleanWords.find( cleanWord => cleanWord.length === longest)
+//    return matchingWord
+// }
+
+// const sentence = "Coding is super fun and challenging!";
+
+// console.log(avgLength(sentence), )
+
+
+
+function longestWord(string) {
+  const words = string.replace(/[^\w\s]/g, '').split(" ");
+  let longest = "";
+  
+  for (let word of words) {
+    if (word.length > longest.length) {
+      longest = word;
+    }
+  }
+
+  return longest;
 }
 
 const sentence = "Coding is super fun and challenging!";
-
-console.log(avgLength(sentence), )
-
-
+console.log(longestWord(sentence)); // "challenging"
