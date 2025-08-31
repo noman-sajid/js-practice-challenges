aa65dd44a047f3a1f7e1b35df3a95a85496b0e65
noman-sajid <noman15072002@gmail.com>
2025-08-31 19:43:16 +0500

    using functions

--- Problem-31/Solution.js
+++ Problem-31/Solution.js
@@ -1,31 +1,50 @@
-//Using frequency counter pattern --Loops
-function areAnagrams(str1, str2) {
-  let cleanStr1 = str1.toLowerCase().replace(/[^\w]/g, "").split("");
-  let cleanStr2 = str2.toLowerCase().replace(/[^\w]/g, "").split("");
+// //Using frequency counter pattern --Loops
+// function areAnagrams(str1, str2) {
+//   let cleanStr1 = str1.toLowerCase().replace(/[^\w]/g, "").split("");
+//   let cleanStr2 = str2.toLowerCase().replace(/[^\w]/g, "").split("");
 
-  const freq1 = {};
-  const freq2 = {};
+//   const freq1 = {};
+//   const freq2 = {};
 
-  for (let char of cleanStr1) {
-    freq1[char] = (freq1[char] || 0) + 1;
-  }
+//   for (let char of cleanStr1) {
+//     freq1[char] = (freq1[char] || 0) + 1;
+//   }
 
-  for (let char of cleanStr2) {
-    freq2[char] = (freq2[char] || 0) + 1;
-  }
+//   for (let char of cleanStr2) {
+//     freq2[char] = (freq2[char] || 0) + 1;
+//   }
 
-  if (Object.keys(freq1).length !== Object.keys(freq2).length) {
-    return false;
-  }
+//   if (Object.keys(freq1).length !== Object.keys(freq2).length) {
+//     return false;
+//   }
 
-  for (let char in freq1) {
-    if (freq1[char] !== freq2[char]) {
-      return false;
-    }
-  }
+//   for (let char in freq1) {
+//     if (freq1[char] !== freq2[char]) {
+//       return false;
+//     }
+//   }
 
-  return true;
+//   return true;
+// }
+
+// console.log(areAnagrams("listen", "silent")); // true
+// console.log(areAnagrams("rail safety", "fairy tales")); // true
+// console.log(areAnagrams("hello", "world")); // false
+
+
+
+
+//Using functions 
+
+function areAnagrams(str1, str2){
+    const cleanStr1 = str1.toLowerCase().replace(/[^\w]/g, "").split("").sort().join("");
+    const cleanStr2 = str2.toLowerCase().replace(/[^\w]/g, "").split("").sort().join("");
+    return cleanStr1 === cleanStr2;
+
 }
+
+
+
 
 console.log(areAnagrams("listen", "silent")); // true
 console.log(areAnagrams("rail safety", "fairy tales")); // true
