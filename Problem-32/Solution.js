26cb5bc28ab60fa4728cb4bb01d9d2994f9aa66d
noman-sajid <noman15072002@gmail.com>
2025-08-31 21:56:39 +0500

    Problem-32-methods based

--- Problem-32/Solution.js
+++ Problem-32/Solution.js
@@ -1,20 +1,34 @@
 //Using loops
+// function firstUniqueChar(str) {
+//   const cleanStr = str.toLowerCase().replace(/[^\w]/g, "");
+//   const freq = {};
+
+//   for (const char of cleanStr) {
+//     freq[char] = (freq[char] || 0) + 1;
+//   }
+
+//   for (const char of cleanStr) {
+//     if (freq[char] === 1) return char;
+//   }
+
+//   return null;
+// }
+
+
+// firstUniqueChar("swiss")       // "w"
+// firstUniqueChar("aabb")        // null
+// firstUniqueChar("Hello World") // "h"
+
+
+//using functions and methods
+
 function firstUniqueChar(str) {
   const cleanStr = str.toLowerCase().replace(/[^\w]/g, "");
-  const freq = {};
-
-  for (const char of cleanStr) {
-    freq[char] = (freq[char] || 0) + 1;
-  }
-
-  for (const char of cleanStr) {
-    if (freq[char] === 1) return char;
-  }
-
-  return null;
+  const chars = [...cleanStr];
+  const freq = chars.reduce((acc, char) => {
+    acc[char] = (acc[char] || 0) + 1;
+    return acc;
+  }, {});
+  const uniqueChar = chars.find(char => freq[char] === 1);      
+  return uniqueChar || null;
 }
-
-
-firstUniqueChar("swiss")       // "w"
-firstUniqueChar("aabb")        // null
-firstUniqueChar("Hello World") // "h"
