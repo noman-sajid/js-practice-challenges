8a0a8a2a99fce4f958ece7d13d9ae38e1ea404d4
noman-sajid <noman15072002@gmail.com>
2025-09-03 22:10:38 +0500

    Problem-34-2

--- Problem-34/PROBLEM.md
+++ Problem-34/PROBLEM.md
@@ -2,3 +2,8 @@ Write a function called isEvenOrOdd that takes one number as an argument. The fu
 
 
 Create a function called findLargestNumber that takes an array of numbers as an argument. The function should find and return the largest number in the array. You should not use any built-in Math functions like Math.max().
+
+
+
+
+Write a function named reverseString that takes a string as an argument and returns the string reversed.
--- Problem-34/Solution.js
+++ Problem-34/Solution.js
@@ -13,11 +13,19 @@
 
 
 
-function findLargestNumber(numbers){
-    let largest = numbers[0];
-    for(let i = 1; i < numbers.length; i++){
-        if(numbers[i] > largest){
-            largest = numbers[i];
-        }
+// function findLargestNumber(numbers){
+//     let largest = numbers[0];
+//     for(let i = 1; i < numbers.length; i++){
+//         if(numbers[i] > largest){
+//             largest = numbers[i];
+//         }
+//     }
+// }
+
+
+function reverseString(str){
+    let reversed = "";
+    for(let i = str.length - 1; i >= 0; i--){
+        reversed += str[i];
     }
 }
