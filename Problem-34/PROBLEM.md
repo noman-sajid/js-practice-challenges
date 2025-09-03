fe93f81b88cd768ce650b3b2c48c8153738e8f73
noman-sajid <noman15072002@gmail.com>
2025-09-03 22:08:33 +0500

    Problem-34-2

--- Problem-34/PROBLEM.md
+++ Problem-34/PROBLEM.md
@@ -1,1 +1,4 @@
 Write a function called isEvenOrOdd that takes one number as an argument. The function should return the string "Even" if the number is even and "Odd" if it's odd.
+
+
+Create a function called findLargestNumber that takes an array of numbers as an argument. The function should find and return the largest number in the array. You should not use any built-in Math functions like Math.max().
--- Problem-34/Solution.js
+++ Problem-34/Solution.js
@@ -1,12 +1,23 @@
-function isEvenOrOdd(number){
-    if(number % 2 === 0){
-        return "Even"
-    } else {
-        return "Odd"
-    }
+// function isEvenOrOdd(number){
+//     if(number % 2 === 0){
+//         return "Even"
+//     } else {
+//         return "Odd"
+//     }
   
-}
+// }
 
-console.log(isEvenOrOdd(4)) // "Even"
-console.log(isEvenOrOdd(7)) // "Odd"
-console.log(isEvenOrOdd(0)) // "Even"
+// console.log(isEvenOrOdd(4)) // "Even"
+// console.log(isEvenOrOdd(7)) // "Odd"
+// console.log(isEvenOrOdd(0)) // "Even"
+
+
+
+function findLargestNumber(numbers){
+    let largest = numbers[0];
+    for(let i = 1; i < numbers.length; i++){
+        if(numbers[i] > largest){
+            largest = numbers[i];
+        }
+    }
+}
