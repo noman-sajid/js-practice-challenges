89db5961c01d57c83e1b67bb5877fd2a7f38036c
noman-sajid <noman15072002@gmail.com>
2025-12-29 06:43:52 +0500

    Problem-34-Practice-3

--- Problem-34/problems.js
+++ Problem-34/problems.js
@@ -1,12 +1,12 @@
 // Calculate the sum of numbers from 1 to 10
 
-let sum = 0;
-for(let i = 1; i <= 10; i++){
+// let sum = 0;
+// for(let i = 1; i <= 10; i++){
   
-  sum += i
+//   sum += i
       
-}
-console.log(sum)
+// }
+// console.log(sum)
 
 
 // Calculate 10! 
@@ -20,3 +20,17 @@ for(let i = 1; i <= 10; i++){
     
 }
 console.log(factorial)
+
+
+
+//Calculate the sum of odd numbers greater than 10 and less or equal than 30
+
+
+
+let sum = 0;
+for(let i = 11; i <= 30; i += 2){
+   sum += i;
+}
+
+console.log(sum)
+
